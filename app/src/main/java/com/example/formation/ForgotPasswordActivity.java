cf1bf46a27c1139f8bead7d411f31134ace701fb
TUF <fatma.bouokazine@gmail.com>
2023-12-22 13:43:38 +0100

    AUTH & PROFILE DONE

--- app/src/main/java/com/example/formation/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/formation/ForgotPasswordActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -22,6 +23,8 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     private EditText email;
     private FirebaseAuth firebaseAuth;
 
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +34,15 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         btnReset = findViewById(R.id.btnResetPass);
         email = findViewById(R.id.emailForgetPass);
         firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
 
         goToSignInActivity.setOnClickListener(v -> {
             startActivity(new Intent(ForgotPasswordActivity.this, SignInActivity.class));
 
         });
         btnReset.setOnClickListener(view -> {
+            progressDialog.setMessage("Please wait ...!");
+            progressDialog.show();
             firebaseAuth.sendPasswordResetEmail(email.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
@@ -47,7 +53,8 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
                     }
                     else{
-                        Toast.makeText(ForgotPasswordActivity.this, "", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(ForgotPasswordActivity.this, "ERROR!", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
                     }
 
                 }
--- app/src/main/java/com/example/formation/SignInActivity.java
+++ app/src/main/java/com/example/formation/SignInActivity.java
@@ -1,37 +1,56 @@
 package com.example.formation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
 
-    private TextView goToForgetPassword;
+    private TextView goToForgotPassword;
     private  TextView goToSignUpACT;
 
 
     private EditText email, password;
     private Button btnSignIn;
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private CheckBox remember;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        goToForgetPassword = findViewById(R.id.goToForgetPassword);
+        goToForgotPassword = findViewById(R.id.goToForgotPassword);
         goToSignUpACT = findViewById(R.id.goToSignUp);
         email=findViewById(R.id.emailSignIn);
         password=findViewById(R.id.passwordSignIn);
         btnSignIn=findViewById(R.id.btnSignIn);
+        remember=findViewById(R.id.remember);
 
-        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuth= FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
 
 
-        goToForgetPassword.setOnClickListener(v -> {
-            startActivity(new Intent(SignInActivity.this, ForgetPassword.class));
+        goToForgotPassword.setOnClickListener(v -> {
+            startActivity(new Intent(SignInActivity.this, ForgotPasswordActivity.class));
 
         });
         goToSignUpACT = findViewById(R.id.goToSignUp);
@@ -42,6 +61,8 @@ public class SignInActivity extends AppCompatActivity {
 
         });
         btnSignIn.setOnClickListener(view -> {
+            progressDialog.setMessage("Please wait ...!");
+            progressDialog.show();
             firebaseAuth.signInWithEmailAndPassword(email.getText().toString().trim(),password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
@@ -49,23 +70,56 @@ public class SignInActivity extends AppCompatActivity {
                         checkEmailVerification();
                     }
                     else {
+                        progressDialog.dismiss();
                         Toast.makeText(SignInActivity.this, "Please verify that your data is correct!", Toast.LENGTH_LONG).show();
                     }
 
                 }
+
+
             });
 
         });
+        SharedPreferences preferences=getSharedPreferences("checkbox",MODE_PRIVATE);
+        boolean isChecked= preferences.getBoolean("remember",false);
+        if (isChecked){
+            startActivity(new Intent(SignInActivity.this, ProfileActivity.class));
+        }
+        else {
+            Toast.makeText(this, "Please login!", Toast.LENGTH_SHORT).show();
+        }
+        remember.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (compoundButton.isChecked()){
+                    SharedPreferences preferences=getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putBoolean("remember",true);
+                    editor.apply();
+                }
+                else{
+                    SharedPreferences preferences=getSharedPreferences("checkbox",MODE_PRIVATE);
+                    SharedPreferences.Editor editor=preferences.edit();
+                    editor.putBoolean("remember",false);
+                    editor.apply();
+
+                }
+
+            }
+        });
     }
+
 
     private void checkEmailVerification() {
         FirebaseUser user=firebaseAuth.getCurrentUser();
         if(user.isEmailVerified()){
             startActivity(new Intent(SignInActivity.this,HomeActivity.class));
+            progressDialog.dismiss();
         }
         else{
             Toast.makeText(this, "Please verify your email!", Toast.LENGTH_SHORT).show();
             firebaseAuth.signOut();
+            progressDialog.dismiss();
         }
     }
 
